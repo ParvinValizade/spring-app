a6036a9770ab16a01e5e4e3b12999b7d42bd8071
ElvinMasmaliyev <elvin.mesmaliyev.99@mail.ru>
2025-04-06 21:13:49 +0400

    getUser

--- src/main/java/paputu/company/az/service/UserService.java
+++ src/main/java/paputu/company/az/service/UserService.java
@@ -3,9 +3,17 @@ package paputu.company.az.service;
 import paputu.company.az.dto.request.CreateUserRequest;
 import paputu.company.az.dto.response.UserResponse;
 
+import java.util.List;
+
 
 public interface UserService {
 
 
     UserResponse createUser(CreateUserRequest userRequest);
+
+    UserResponse getUserByEmail(String email);
+
+    List<UserResponse> getAllUsers();
+
+    void deleteUserByEmail(String email);
 }
